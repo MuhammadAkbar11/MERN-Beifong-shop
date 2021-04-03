655258d43362424f4861500354751e4fd5cb4341
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-04-03 15:20:37 +0700

    2. React-Bootstrap Setup, Header & Footer Components

--- frontend/src/app/containers/Home.jsx
+++ frontend/src/app/containers/Home.jsx
@@ -1,40 +1,20 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'reactstrap';
+import { Button, Container } from 'react-bootstrap';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 const Home = () => {
   return (
     <>
-      <Container
-        fluid
-        className='border  '
-        style={{
-          height: '100vh',
-        }}
-      >
-        <Row style={{ height: '100%' }}>
-          <Col>
-            <Card className='border-0 shadow-none d-flex flex-column h-100 justify-content-center align-items-center '>
-              <h1 className='text-dark'>
-                Webpack, React and Babel Boilerplate.
-              </h1>
-              <h3 className='text-dark'>
-                Support{' '}
-                <a className='text-purple' href='https://getbootstrap.com/'>
-                  Bootstrap
-                </a>
-              </h3>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-
-      {/* <div className=''>
-        <h1 className=''>Webpack, React and Babel Boilerplate</h1>
-        <h3 className=''>
-          Support
-          Css Modules and Scss
-        </h3>
-      </div> */}
+      <Header />
+      <main className='py-3'>
+        <Container>
+          {' '}
+          <h1>Beifong</h1>
+          <Button variant='primary'>Shop now</Button>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
