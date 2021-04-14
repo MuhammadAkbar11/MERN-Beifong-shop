9d6fefc478dcfbd97572ba62b8aed0e866b09549
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-04-14 13:28:47 +0700

    added active class

--- frontend/src/app/components/CheckoutSteps.jsx
+++ frontend/src/app/components/CheckoutSteps.jsx
@@ -18,39 +18,50 @@ const proptypes = {
 };
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  const activeClass = 'font-weight-bold text-primary ';
   return (
-    <Nav className=' justify-content-center mb-4 '>
-      <Nav.Item>
+    <Nav className=' justify-content-center mb-5 mt-3 '>
+      <Nav.Item className='mr-1'>
         {step1 ? (
-          <LinkContainer to='/login'>
-            <Nav.Link>Sign in</Nav.Link>
+          <LinkContainer to='/login' className={activeClass}>
+            <Nav.Link>
+              Sign in <i className='fa fa-check-circle ml-1' />
+            </Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Sign In</Nav.Link>
         )}
       </Nav.Item>
-      <Nav.Item>
+      <Nav.Item className='mr-1'>
         {step2 ? (
-          <LinkContainer to='/shipping'>
-            <Nav.Link>Shipping</Nav.Link>
+          <LinkContainer to='/shipping' className={activeClass}>
+            <Nav.Link>
+              Shipping{' '}
+              {step2 && step3 && <i className='fa fa-check-circle ml-1' />}
+            </Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>shipping</Nav.Link>
         )}
       </Nav.Item>
-      <Nav.Item>
+      <Nav.Item className='mr-1'>
         {step3 ? (
-          <LinkContainer to='/payment'>
-            <Nav.Link>Payment</Nav.Link>
+          <LinkContainer to='/payment' className={activeClass}>
+            <Nav.Link>
+              Payment{' '}
+              {step3 && step4 && <i className='fa fa-check-circle ml-1' />}
+            </Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Payment</Nav.Link>
         )}
       </Nav.Item>
-      <Nav.Item>
+      <Nav.Item className='mr-1'>
         {step4 ? (
-          <LinkContainer to='/placeorder'>
-            <Nav.Link>Place Order</Nav.Link>
+          <LinkContainer to='/placeorder' className={activeClass}>
+            <Nav.Link>
+              Place Order <i className='fa fa-check-circle ml-1' />
+            </Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Place Order</Nav.Link>
