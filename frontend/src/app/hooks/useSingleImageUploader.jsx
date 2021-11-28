07a534fb1f40d8dad56562c456928282521d446d
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-11-28 13:03:02 +0700

    crete a hook for handle image upload

---
+++ frontend/src/app/hooks/useSingleImageUploader.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const useSingleImageUploader = ({ defaultImage }) => {
+  const [image, setImage] = React.useState(defaultImage || null);
+
+  const handleFile = files => {
+    const newFiles = [...files].map(file => {
+      return {
+        filename: file.name,
+        file,
+        url: URL.createObjectURL(file),
+      };
+    });
+    setImage(newFiles[0]);
+  };
+
+  const handleReset = () => setImage(null);
+
+  return { image, handleFile, handleReset };
+};
+
+useSingleImageUploader.defaultProps = {
+  defaultImage: '',
+};
+
+export default useSingleImageUploader;
