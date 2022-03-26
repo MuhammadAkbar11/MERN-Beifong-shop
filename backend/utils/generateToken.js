65bfd515ed359d47f92be7db025353de46bb884e
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-26 20:00:25 +0700

    utils for generate access token and refresh token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,9 +1,36 @@
 import jwt from "jsonwebtoken";
+import {
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+} from "../configs/constants.js";
 
 const generateToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "3d",
   });
+};
+
+export const generateAccessToken = userInfo => {
+  return jwt.sign(
+    {
+      UserInfo: {
+        username: userInfo.name,
+        // roles: roles,
+      },
+    },
+    ACCESS_TOKEN_SECRET,
+    { expiresIn: "30s" }
+  );
+};
+
+export const generateRefreshToken = userInfo => {
+  return jwt.sign(
+    { username: userInfo.name, id: userInfo._id },
+    REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "1d",
+    }
+  );
 };
 
 export default generateToken;
