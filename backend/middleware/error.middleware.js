0a4d66b007b9b82d107cb9c159f16b3b8389cbe9
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-27 22:34:14 +0700

    change status code for not found error

--- backend/middleware/error.middleware.js
+++ backend/middleware/error.middleware.js
@@ -4,7 +4,7 @@ import ResponseError from "../utils/responseError.js";
 
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found = ${req.originalUrl}`);
-  res.status(400);
+  res.status(404);
   next(error);
 };
 
