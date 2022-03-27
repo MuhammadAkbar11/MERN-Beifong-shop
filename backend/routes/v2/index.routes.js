ca749b4ede775e8bb015ee4389a3f2f4cb712d87
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-27 22:35:00 +0700

    replace refresh token routes with get session

--- backend/routes/v2/index.routes.js
+++ backend/routes/v2/index.routes.js
@@ -1,13 +1,17 @@
 import { PREFIX_VERSION } from "../../configs/constants.js";
-import { handleRefreshToken } from "../../controllers/v2/auth.controller.js";
+import { getSession } from "../../controllers/v2/auth.controller.js";
 import UserRoutes from "./user.routes.js";
 
 function AppRoutesV2(app) {
   app.get(`${PREFIX_VERSION}`, (req, res) => {
-    res.json({ status: true, message: "Welcome to beifong shop Api V2" });
+    res.json({
+      status: true,
+      user: req.user,
+      message: "Welcome to beifong shop Api V2",
+    });
   });
 
-  app.get(`${PREFIX_VERSION}/refresh`, handleRefreshToken);
+  app.get(`${PREFIX_VERSION}/session`, getSession);
 
   // user Routes
   UserRoutes(app, `${PREFIX_VERSION}/users`);
