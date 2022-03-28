6d8e39ca72a61b2c60e5869f09eb0a565b917f02
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-28 15:47:41 +0700

    page transition component

---
+++ frontend/src/app/components/PageTransition.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PageTransition = () => {
+  return (
+    <div className='page-transition bg-slate'>
+      <div className='d-flex justify-content-center align-items-center h-100  '>
+        <h1>Loading ...</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PageTransition;
