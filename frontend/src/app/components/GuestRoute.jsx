7f0ff1fa07bf2070deb2bdccb91ece83e27f41dc
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-28 22:16:09 +0700

    fix bug in GuestRoute Comp when there no session but its auto redirect to home page when page was refresh

--- frontend/src/app/components/GuestRoute.jsx
+++ frontend/src/app/components/GuestRoute.jsx
@@ -5,28 +5,32 @@ import { getSessionAction } from '../actions/v2/session.actions';
 import PageTransition from './PageTransition';
 
 const GuestRoute = ({ component: Component, restricted, ...rest }) => {
-  const { loading, userInfo } = useSelector(state => state.session);
+  const { loading, userInfo, status: sessionStatus } = useSelector(
+    state => state.session
+  );
 
   const dispatch = useDispatch();
 
-  React.useLayoutEffect(() => {
-    dispatch(getSessionAction());
-  }, [dispatch]);
-  console.log(loading);
+  React.useEffect(() => {
+    if (!sessionStatus) {
+      dispatch(getSessionAction());
+    }
+  }, [dispatch, sessionStatus]);
+
   return (
     <Route
       {...rest}
       render={props => {
-        return loading ? (
-          <PageTransition />
-        ) : (
+        return !loading ? (
           <>
-            {userInfo !== null && restricted ? (
+            {userInfo && restricted ? (
               <Redirect to='/' />
             ) : (
               <Component {...props} />
             )}
           </>
+        ) : (
+          <PageTransition />
         );
       }}
     />
