23820cfe24241d07c2ad821c27b5011305cc320b
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-28 22:11:27 +0700

    remove unnecessary function in generateToken Util's file

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,37 +1,9 @@
 import jwt from "jsonwebtoken";
-import {
-  ACCESS_TOKEN_SECRET,
-  REFRESH_TOKEN_SECRET,
-} from "../configs/constants.js";
 
 const generateToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "3d",
   });
-};
-
-export const generateAccessToken = userInfo => {
-  return jwt.sign(
-    {
-      UserInfo: {
-        name: userInfo.name,
-        id: userInfo._id,
-        // roles: roles,
-      },
-    },
-    ACCESS_TOKEN_SECRET,
-    { expiresIn: "30s" }
-  );
-};
-
-export const generateRefreshToken = userInfo => {
-  return jwt.sign(
-    { name: userInfo.name, id: userInfo._id },
-    REFRESH_TOKEN_SECRET,
-    {
-      expiresIn: "1d",
-    }
-  );
 };
 
 export default generateToken;
