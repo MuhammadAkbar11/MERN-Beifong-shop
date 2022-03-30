20771a850c16ead4b0d9d412ed51ef3f2b433b56
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-31 00:12:23 +0700

    create order routes v2

---
+++ backend/routes/v2/order.routes.js
@@ -0,0 +1,20 @@
+import { protect } from "../../middleware/v2/auth.middleware.js";
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  getOrders,
+  updateOrdertoDelivered,
+  updateOrdertoPaid,
+} from "../../controllers/order.controller.js";
+
+function OrderRoutes(app, prefix) {
+  // app.get(`${prefix}/`, protect, adminProtect, getOrders);
+  app.post(`${prefix}/`, protect, addOrderItems);
+  app.get(`${prefix}/myorders`, protect, getMyOrders);
+  app.get(`${prefix}/details/:id`, protect, getOrderById);
+  app.put(`${prefix}/:id/pay`, protect, updateOrdertoPaid);
+  // app.put("/:id/deliver", protect, adminProtect, updateOrdertoDelivered);
+}
+
+export default OrderRoutes;
