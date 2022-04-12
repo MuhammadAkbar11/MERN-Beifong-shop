909ae9eadedf62af8dae16107dd2cc672d67db25
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-04-12 23:28:02 +0700

    remove fetch session enpoint in private & public Route components

--- frontend/src/app/components/RouteGuest.jsx
+++ frontend/src/app/components/RouteGuest.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import { getSessionAction } from '../actions/v2/session.actions';
 import PageTransition from './PageTransition';
 
 const RouteGuest = ({ component: Component, restricted, ...rest }) => {
-  const { loading, userInfo, status: sessionStatus } = useSelector(
-    state => state.session
-  );
-
-  const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    if (!sessionStatus) {
-      dispatch(getSessionAction());
-    }
-  }, [dispatch, sessionStatus]);
+  const { loading, userInfo } = useSelector(state => state.session);
 
   return (
     <Route
--- frontend/src/app/components/RoutePrivate.jsx
+++ frontend/src/app/components/RoutePrivate.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import { getSessionAction } from '../actions/v2/session.actions';
-import { userLogoutAction } from '../actions/v2/user.actions';
+import { userLogoutAction } from '@actions/v2/user.actions';
 import PageTransition from './PageTransition';
 
 const RoutePrivate = ({ component: Component, restricted, ...rest }) => {
-  const { loading, userInfo, status: sessionStatus, isLogout } = useSelector(
-    state => state.session
-  );
+  const { loading, userInfo, isLogout } = useSelector(state => state.session);
 
   const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    if (!sessionStatus) {
-      dispatch(getSessionAction());
-    }
-  }, [dispatch, sessionStatus]);
 
   React.useEffect(() => {
     if (isLogout) {
