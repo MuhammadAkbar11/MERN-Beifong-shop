8dca46dfc47db4960dafb04ca2b9dc357f532db7
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-04-23 22:44:49 +0700

    (v2) get, delete, put users routes Endpoint

--- backend/routes/v2/user.routes.js
+++ backend/routes/v2/user.routes.js
@@ -1,5 +1,9 @@
 import {
+  deleteUser,
+  getUserById,
   getUserProfile,
+  getUsers,
+  updateUser,
   updateUserProfile,
   userPostCart,
   userRemoveCart,
@@ -9,14 +13,16 @@ import {
   postLogout,
   postRegister,
 } from "../../controllers/auth.controller.js";
-import { protect } from "../../middleware/v2/auth.middleware.js";
+import { adminProtect, protect } from "../../middleware/v2/auth.middleware.js";
 import loginValidation from "../../middleware/validations/login.validation.js";
 import registerValidation from "../../middleware/validations/register.validation.js";
+import userUpdateValidation from "../../middleware/validations/user.validation.js";
 
 function UserRoutes(app, prefix) {
+  app.route(`${prefix}`).get(protect, adminProtect, getUsers);
   app.route(`${prefix}/login`).post([loginValidation], postLogin);
   app.route(`${prefix}/register`).post([registerValidation], postRegister);
-  app.route(`${prefix}/logout`).post(postLogout);
+  app.route(`${prefix}/logout`).post(protect, postLogout);
   app
     .route(`${prefix}/profile`)
     .get(protect, getUserProfile)
@@ -24,6 +30,12 @@ function UserRoutes(app, prefix) {
 
   app.route(`${prefix}/cart`).post(protect, userPostCart);
   app.route(`${prefix}/cart/delete`).post(protect, userRemoveCart);
+
+  app
+    .route("/:id")
+    .get(protect, adminProtect, getUserById)
+    .delete(protect, adminProtect, deleteUser)
+    .put(protect, adminProtect, [userUpdateValidation], updateUser);
 }
 
 export default UserRoutes;
